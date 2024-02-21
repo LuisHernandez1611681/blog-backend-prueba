3d2dd26643f266fb958f0f89ae70fad562e710c4
luisHdz <luishernandez1611681@gmail.com>
2024-02-20 19:59:04 -0600

    Endpoint para la creacion de articulos

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,13 +1,29 @@
 const pool = require('../database');
 
 // Obtener todos los artículos
-exports.getAllArticles = (req, res) => {
-    pool.query('SELECT * FROM article', (error, result) => {
-        if (error) {
-            console.error('Error getAllArticles: ', error);
-            res.status(500).json({ error: 'Internal Server error' });
-        } else {
-            res.json(result.rows);
-        }
-    });
+exports.getAllArticles = async (req, res) => {
+  try {
+    const query = 'SELECT * FROM article';
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error getAllArticles: ', error);
+    res.status(500).json({ error: 'Internal Server error' });
+  }
+};
+
+
+// Insertar un nuevo artículo
+exports.createArticle = async (req, res) => {
+  try {
+    const { title, author, created_at, content } = req.body;
+    const query = 'INSERT INTO article (title, author, created_at, content) VALUES ($1, $2, $3, $4) RETURNING *';
+    const values = [title, author, created_at, content];
+
+    const result = await pool.query(query, values);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error createArticle: ', error);
+    res.status(500).json({ error: 'Internal Server error' });
+  }
 };
--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,7 @@ const articlesController = require('../controllers/articlesController');
 // Obtener todos los artículos
 router.get('/', articlesController.getAllArticles);
 
+// Insertar un nuevo artículo
+router.post('/', articlesController.createArticle);
+
 module.exports = router;
